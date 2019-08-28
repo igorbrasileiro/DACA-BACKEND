936dc679c421ec7ac3401c30c018dd3774c40285
igorbrasileiro <brasileiro456@gmail.com>
2019-08-27 22:13:38 -0300

    add createDepute resolver

--- src/graphql/resources/depute/depute.resolver.ts
+++ src/graphql/resources/depute/depute.resolver.ts
@@ -1,12 +1,44 @@
 import { Transaction } from 'sequelize/types';
 import { DbConnection } from '../../../interfaces/DbConnectionInterface';
+import { User } from '../../../models/User';
 
 export const resolvers = {
   Depute: {
     // person:
   },
   Mutation: {
-    // createDepute:
+    createDepute: (parent, { dni, createdAt }, { db }:{ db: DbConnection }) => {
+      return db.sequelize.transaction((t: Transaction) => {
+        return (<any>db.User).findOne({ where: { dni } }).then((user: User) => {
+          if (!user) {
+            return new Error();
+          }
+          if (createdAt.length !== 8) {
+            return new Error();
+          }
+          const formatedCreatedAt = new Date(
+              createdAt.slice(4, 8),
+              createdAt.slice(2, 4),
+              createdAt.slice(0, 2),
+            );
+          const dateNow = new Date();
+          if (dateNow > formatedCreatedAt) {
+            return new Error();
+          }
+
+          // console.log('asduasdhasudha', formatedCreatedAt);
+          return (<any>db.Depute).create(
+            {
+              createdAt: formatedCreatedAt,
+              person: user.id,
+            },
+            {
+              transaction: t,
+            },
+          );
+        });
+      });
+    },
   },
   Query: {
     // depute:
--- src/interfaces/BaseModelInterface.ts
+++ src/interfaces/BaseModelInterface.ts
@@ -1,10 +1,12 @@
 import { Model } from 'sequelize';
 import { User } from '../models/User';
 import { State } from '../models/State';
+import { Depute } from '../models/Depute';
 
 export interface BaseModelInterface extends Model {
   User?: User;
   State?: State;
+  Depute?: Depute;
 
   // atributo opcional, serve para criar metodos de instancias
   prototype?;
