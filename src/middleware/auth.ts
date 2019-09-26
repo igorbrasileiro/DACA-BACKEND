3d37acb11633e6df8a36aba5012770c838f3875c
igorbrasileiro <brasileiro456@gmail.com>
2019-09-26 00:00:22 -0300

    add .env file

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,14 +1,15 @@
 import { sign } from 'jsonwebtoken';
+import ENV from '../config/environment';
 import { genSaltSync, compareSync } from 'bcryptjs';
 import { ExtractJwt } from 'passport-jwt';
 import { DbConnection } from '../interfaces/DbConnectionInterface';
 
 export const params = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'ALTERAR', // process.env.SECRET_KEY
+  secretOrKey: ENV.SECRET_KEY, // process.env.SECRET_KEY
 };
 
-const SALT_FACTOR = 10; // process.env.SALT_FACTOR
+const SALT_FACTOR = ENV.SALT_FACTOR; // process.env.SALT_FACTOR
 export const SALT = genSaltSync(SALT_FACTOR);
 
 export const getToken = async (dni: string, password: string,  db: DbConnection) => {
